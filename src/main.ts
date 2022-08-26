eff4e350d5e527cacb24eddc3544ef812801c89e
Poitier Stringer <stringerpoitier@gmail.com>
2022-08-26 16:45:19 -0700

    add light

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { DepthFormat } from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-
 interface CubeDimensions {
   width: number;
   height: number;
@@ -22,6 +14,17 @@ interface Vect {
   y: number;
   z: number;
 }
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+const pointLight = new THREE.PointLight(0xffffff);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(pointLight, ambientLight);
+
+pointLight.position.set(0, 0, 1);
 
 type Direction = 'left' | 'right' | 'forward' | 'back' | 'up' | 'down'
 
